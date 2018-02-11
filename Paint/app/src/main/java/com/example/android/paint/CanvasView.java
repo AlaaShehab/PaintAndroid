f607877866b6f9397caa7a8250237d74340d4cf8
saraheldafrawy <sarah.eldafrawy257@gmail.com>
2018-02-11 22:23:09 +0200

    Undo

--- Paint/app/src/main/java/com/example/android/paint/CanvasView.java
+++ Paint/app/src/main/java/com/example/android/paint/CanvasView.java
@@ -14,8 +14,6 @@ import android.view.View;
 import com.example.android.paint.draw.Shape;
 import com.example.android.paint.touchStrategy.TouchStrategy;
 
-import java.util.List;
-
 /**
  * Created by Alaa on 2/8/2018.
  */
@@ -25,9 +23,11 @@ public class CanvasView extends View {
     private Paint paint;
     private Canvas canvas;
     private Shape shapeInUse;
+    private Shape[] shapes;
+    private boolean flag = false;
 
     public CanvasView(Context context, AttributeSet sets) {
-        super(context);
+        super(context, sets);
         setupView();
     }
 
@@ -35,18 +35,19 @@ public class CanvasView extends View {
 //        TODO check other methods as paint.attributes
 //        TODO check rest of the setups in link
         paint = new Paint();
-        paint.setColor(Color.BLACK);
+        paint.setColor(Color.CYAN);
         paint.setStrokeWidth(4);
-        //////
-        paint.setAntiAlias(true);
-        paint.setStrokeWidth(20);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeJoin(Paint.Join.ROUND);
-        paint.setStrokeCap(Paint.Cap.ROUND);
+        ////// valid only for pencil
+//        paint.setAntiAlias(true);
+//        paint.setStrokeWidth(20);
+//        paint.setStyle(Paint.Style.STROKE);
+//        paint.setStrokeJoin(Paint.Join.ROUND);
+//        paint.setStrokeCap(Paint.Cap.ROUND);
     }
-    public void setPaint (Paint paint) {
+
+    public void setPaint(Paint paint) {
         this.paint = paint;
-        //TODO implement it in MainActivity
+        //TODO implement change in paint in MainActivity
     }
 
     @Override
@@ -56,25 +57,39 @@ public class CanvasView extends View {
         Log.e("OnSizeChange", "Hi");
         this.canvas = new Canvas(bitmap);
         canvas.save();
-        ((MainActivity) getContext()).setDrawingCanvas(canvas);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        if (shapeInUse != null) {
-            this.canvas.restore();
-            shapeInUse.draw(this.canvas, paint);
+        if (flag) {
+            this.canvas.drawColor(Color.WHITE);
             this.canvas.save();
+            for(int i = 0; i< shapes.length; i++) {
+                this.canvas.restore();
+                Log.e("From Loop", ""+flag);
+                shapes[i].draw(this.canvas,shapes[i].getPaint());
+                this.canvas.save();
+            }
+            flag = false;
+            Log.e("From refresh", ""+flag);
+        } else {
+            if (shapeInUse != null) {
+                this.canvas.restore();
+                shapeInUse.draw(this.canvas, paint);
+                this.canvas.save();
+            }
         }
         if (bitmap != null) {
-            canvas.drawBitmap(bitmap,0,0,null);
+            canvas.drawBitmap(bitmap, 0, 0, null);
         }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         //TODO rest of touch events (resize, delete, select, etc)
-        Point pt = new Point((int) event.getX(), (int)event.getY());
+        //TODO if shapeInUse null donc, 3amalna select
+        //TODO select btn set it by null after each event
+        Point pt = new Point((int) event.getX(), (int) event.getY());
         String buttonSelected = ((MainActivity) getContext()).getBtnSelected();
         if (shapeInUse == null) {
             shapeInUse = FactoryShapes.getShape(buttonSelected);
@@ -91,12 +106,11 @@ public class CanvasView extends View {
             case MotionEvent.ACTION_UP:
                 strategy.actionUp(shapeInUse, pt);
                 try {
-                    ((MainActivity) getContext()).addShape((Shape)shapeInUse.clone());
+                    ((MainActivity) getContext()).addShape((Shape) shapeInUse.clone());
                 } catch (CloneNotSupportedException e) {
                     e.printStackTrace();
                     Log.e("CloneError", "CLONE!!!!");
                 }
-//                ((MainActivity) getContext()).getDrawingEngine().refresh(canvas);
                 shapeInUse = null;
                 break;
             default:
@@ -106,16 +120,14 @@ public class CanvasView extends View {
         return true;
     }
 
-    public Canvas getCanvas() {
-        return this.canvas;
-    }
-    public void clearCanvas() {
-        this.canvas.drawColor(Color.CYAN);
+    public void refreshCanvas(Shape[] shapes) {
+        //TODO undo not working correctly in path
+        flag = true;
+        Log.e("From refresh", ""+flag);
+        Log.e("ShapeListLength",""+shapes.length);
+        this.shapes = shapes;
+        Log.e("ShapeListLength",""+this.shapes.length);
         invalidate();
     }
+
 }
-
-
-//TODOs
-//TODO if shapeInUse null donc, 3amalna select
-//TODO select btn set it by null after each event
--- Paint/app/src/main/java/com/example/android/paint/MainActivity.java
+++ Paint/app/src/main/java/com/example/android/paint/MainActivity.java
@@ -1,14 +1,9 @@
 package com.example.android.paint;
 
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-
 import com.example.android.paint.draw.Model.DrawingEngine;
 import com.example.android.paint.draw.Shape;
 
@@ -17,67 +12,39 @@ public class MainActivity extends AppCompatActivity {
     private String btnSelected;
     /**Suppose DrawingEngine a singleton**/
     private DrawingEngine drawingEngine;
-    private CanvasView canvasView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         drawingEngine = new DrawingEngine();
-        ((CanvasView)findViewById(R.id.canvas_layout)).clearCanvas();
-    }
-
-    public void setDrawingCanvas (Canvas canvas) {
-//        Log.e("HI FROM SetCav", "  HI Again");
-//        this.canvas = canvas;
-    }
-
-    public DrawingEngine getDrawingEngine () {
-        return this.drawingEngine;
     }
 
     public void setBtnSelected(View view) {
-//        this.btnSelected = ((Button)view).toString(); // check function
-        this.btnSelected = "path";
-        System.out.println(btnSelected); //for test
+//        this.btnSelected = view.getTag().toString();
+        this.btnSelected = "circle";
+//        this.btnSelected = "path";
+        Log.e("btnSelected", btnSelected); //for test
     }
 
     public void undoAction(View view) {
-        Log.e("ana gowa","lessa da5el");
         drawingEngine.undo();
-        Log.e("undo","3adeit undo");
-//        Canvas canvas = ((CanvasView)findViewById(R.id.canvas)).getCanvas();
-        //TODO undoAction
-//        canvasView = (CanvasView)findViewById(R.id.canvas_layout);
-          canvasView.clearCanvas();
-//        canvasView.getCanvas().drawColor(Color.BLACK);
-        Log.e("BLACK","BLACK to7fa");
-        Paint paint = new Paint();
-        paint.setColor(Color.BLACK);
-        paint.setStrokeWidth(4);
-//       canvasView.getCanvas().drawCircle(200, 300, 500, paint);
-        //drawingEngine.refresh(canvas);
-        //Log.e("Refresh","refresh");
-        //canvas.save();
-        //Log.e("saved","canvas");
-
+        ((CanvasView)findViewById(R.id.canvas_layout)).refreshCanvas(drawingEngine.getShapes());
+        //TODO delete refresh
     }
 
-    public void redoAction () {
+    public void redoAction (View view) {
         drawingEngine.redo();
-        //TODO redoAction
-        //clear canvas
-//        drawingEngine.refresh(canvas);
-
+        ((CanvasView)findViewById(R.id.canvas_layout)).refreshCanvas(drawingEngine.getShapes());
     }
 
-    public void saveAction () {
+    public void saveAction (View view) {
         //TODO saveAction
         //Alaa will save shapes
 //        drawingEngine.save();
 
     }
-    public void loadAction () {
+    public void loadAction (View view) {
         //TODO loadAction
         //clear canvas
         //Alaa will load shapes
