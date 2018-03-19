a5707509b04d55834aa18faecfd54fb5cddac978
AlaaShehab <alaa.shehab.95@gmail.com>
2018-03-19 07:22:45 +0200

    I don't remember what I have edited but I have finished all xml layouts + started working on paint palette

--- Paint/app/src/main/java/com/example/android/paint/MainActivity.java
+++ Paint/app/src/main/java/com/example/android/paint/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.android.paint;
 
 import android.annotation.SuppressLint;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
@@ -10,6 +15,7 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import com.example.android.paint.draw.Model.DrawingEngine;
@@ -25,8 +31,8 @@ public class MainActivity extends AppCompatActivity {
      * Suppose DrawingEngine a singleton
      **/
     private DrawingEngine drawingEngine;
-    private PopupWindow popupWindow;
-
+    private PopupWindow shapesPopupWindow;
+    private PopupWindow colorPopupWindow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -73,6 +79,8 @@ public class MainActivity extends AppCompatActivity {
 
     //TODO Copy, Move, Resize (Update methods in DrawingEngine)
 
+
+
     public String getBtnSelected() {
         return btnSelected;
     }
@@ -98,29 +106,27 @@ public class MainActivity extends AppCompatActivity {
         assert inflater != null;
         @SuppressLint("InflateParams") final View popupView = inflater.inflate(R.layout.shapes_popup_window, null);
 
-        int width = RelativeLayout.LayoutParams.WRAP_CONTENT;
-        int height = RelativeLayout.LayoutParams.WRAP_CONTENT;
-        popupWindow = new PopupWindow(popupView, width, height);
-        popupWindow.showAtLocation(findViewById(R.id.activity_main_layout), Gravity.CENTER, 0, 0);
-//
-//        popupWindow.setOutsideTouchable(true);
-//        popupWindow.setFocusable(true);
+        int width = RelativeLayout.LayoutParams.MATCH_PARENT;
+        int height = RelativeLayout.LayoutParams.MATCH_PARENT;
+        shapesPopupWindow = new PopupWindow(popupView, width, height);
+        shapesPopupWindow.showAtLocation(findViewById(R.id.activity_main_layout), Gravity.CENTER, 0, 0);
 
         popupView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                popupWindow.dismiss();
+                shapesPopupWindow.dismiss();
                 return true;
             }
         });
-    //TODO handle clikcing outside the popup window
     }
 
     public void setBtnSelected(View view) {
         this.btnSelected = view.getTag().toString();
-        if (!(this.btnSelected.equals(getString(R.string.path)))) {
-            popupWindow.dismiss();
-        }
+        shapesPopupWindow.dismiss();
+        //TODO commented l7ad ma nfhmha XD
+//        if (!(this.btnSelected.equals(getString(R.string.path)))) {
+//            shapesPopupWindow.dismiss();
+//        }
     }
 
     public String getEditBtnSelected() {
@@ -129,6 +135,28 @@ public class MainActivity extends AppCompatActivity {
 
     public void setEditBtnSelected(View view) {
         this.editBtnSelected = view.getTag().toString();
+    }
+
+    public void setColor (View view) {
+        LinearGradient colors = new LinearGradient(0.f, 0.f, 300.f, 0.0f,
+                new int[] { 0xFF000000, 0xFF0000FF, 0xFF00FF00, 0xFF00FFFF,
+                        0xFFFF0000, 0xFFFF00FF, 0xFFFFFF00, 0xFFFFFFFF},
+                null, Shader.TileMode.CLAMP);
+        ShapeDrawable shape = new ShapeDrawable(new RectShape());
+        shape.getPaint().setShader(colors);
+
+        SeekBar seekBarFont = (SeekBar)findViewById(R.id.seekbar);
+        seekBarFont.setProgressDrawable( (Drawable)shape );
+
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        assert inflater != null;
+        @SuppressLint("InflateParams") final View paletteView = inflater.inflate(R.layout.color_palette_window, null);
+
+        int width = RelativeLayout.LayoutParams.WRAP_CONTENT;
+        int height = RelativeLayout.LayoutParams.WRAP_CONTENT;
+        colorPopupWindow = new PopupWindow(paletteView, width, height);
+        colorPopupWindow.showAtLocation(findViewById(R.id.activity_main_layout), Gravity.CENTER, 0, 0);
 
     }
 }
